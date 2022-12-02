37545b9a4e99fd79124344ff3133670f1c233bd7
SAURABH PRAKASH <74844269+saurabhp8507014288@users.noreply.github.com>
2022-12-02 19:28:08 +0530

    Add files via upload
    intial submit

---
+++ calc.py
@@ -0,0 +1,15 @@
+# to demonstrate module topic
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+print("hello"+__name__)
